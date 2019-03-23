e2a392d469fc4d70b1ab8cb93de0abe9a5146d5f
Eric Zheng <eric.zheng.hz.2016@hotmail.com>
2019-03-23 13:09:08 +0800

    lru

---
+++ algo/LruFunc.py
@@ -0,0 +1,15 @@
+import functools
+
+@functools.lru_cache(maxsize=None)
+def fib(i):
+    if i < 2:
+        return 1
+    return fib(i - 1) + fib(i - 2)
+
+@functools.lru_cache(maxsize=None)
+def C(n,k):
+    if k == 0:
+        return 1
+    if n == 0:
+        return 0
+    return C(n-1,k-1) + C(n-1,k)
