203a7e8e30c3c3cba25525032e59d8c670cedbe0
Eric Zheng <eric.zhi.zheng@gmail.com>
2019-03-25 13:36:50 +0800

    add rm

---
+++ algo/MergeSort.py
@@ -0,0 +1,31 @@
+"""
+time complexity O(N*logN)
+"""
+
+
+def merge(a, b):
+    c = []
+    h, j = 0, 0
+    while j < len(a) and h < len(b):
+        if a[j] < b[h]:
+            c.append(a[j])
+            j += 1
+        else:
+            c.append(b[h])
+            h += 1
+    if j == len(a):
+        for i in b[h:]:
+            c.append(i)
+    else:
+        for i in a[j:]:
+            c.append(i)
+    return c
+
+
+def merge_sort(lists):
+    if len(lists) <= 1:
+        return lists
+    middle = len(lists) // 2
+    left = merge_sort(list[:middle])
+    right = merge_sort(list[middle:])
+    return merge(left, right)
