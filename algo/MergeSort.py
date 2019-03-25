82425764f2f2913d87668a24778fedb090031c05
Eric Zheng <eric.zhi.zheng@gmail.com>
2019-03-25 13:39:00 +0800

    add rm

--- algo/MergeSort.py
+++ algo/MergeSort.py
@@ -29,3 +29,22 @@ def merge_sort(lists):
     left = merge_sort(list[:middle])
     right = merge_sort(list[middle:])
     return merge(left, right)
+
+
+"""
+time complexity O(log2N)
+"""
+
+
+def bin_search(data_list, val):
+    low = 0  # 最小数下标
+    high = len(data_list) - 1  # 最大数下标
+    while low <= high:
+        mid = (low + high) // 2  # 中间数下标
+        if data_list[mid] == val:  # 如果中间数下标等于val, 返回
+            return mid
+        elif data_list[mid] > val:  # 如果val在中间数左边, 移动high下标
+            high = mid - 1
+        else:  # 如果val在中间数右边, 移动low下标
+            low = mid + 1
+    return  # val不存在, 返回None
