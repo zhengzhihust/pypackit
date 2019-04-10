f60342962ceb0e5fb2c369ef467a18cab7f71fe4
Eric Zheng <eric.zhi.zheng@gmail.com>
2019-04-10 10:40:48 +0800

    add nesting method for hard mode

--- codility/Nesting.py
+++ codility/Nesting.py
@@ -51,3 +51,70 @@ def solution(S):
     if count != 0:
         return 0
     return 1
+
+
+"""
+@see https://leetcode.com/problems/valid-parentheses/
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        stack = []
+        length = len(s)
+        if length % 2:
+            return False
+        for c in s:
+            if not stack:
+                stack.append(c)
+                continue
+            top = stack[-1]
+            if top == '(':
+                if c == ')':
+                    stack.pop()
+                else:
+                    stack.append(c)
+            elif top == '[':
+                if c == ']':
+                    stack.pop()
+                else:
+                    stack.append(c)
+            elif top == '{':
+                if c == '}':
+                    stack.pop()
+                else:
+                    stack.append(c)
+            else:
+                return False
+        return not stack
